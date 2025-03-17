cc0cd645dba662bad68f5292248aab3c9acfb060
LE Van Anh Khoa <106988219+AK087204@users.noreply.github.com>
2025-03-17 21:30:28 +0700

    feat: implement article creation and crawler configuration with dependencies

--- BE/src/schemas/article.schema.ts
+++ BE/src/schemas/article.schema.ts
@@ -1,31 +1,56 @@
+// articles/schemas/article.schema.ts
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document, Types } from "mongoose";
 
-@Schema({ collection: "Article", timestamps: true })
-export class Article extends Document {
+export type ArticleDocument = Article & Document;
+
+@Schema({ collection: "Article" }) // Giữ createdAt, updatedAt
+// @Schema({}) // Không giữ createdAt, updatedAt
+export class Article {
   @Prop({ required: true })
   title: string;
 
   @Prop({ required: true })
   link: string;
 
-  @Prop()
-  author?: string;
+  @Prop([String]) // Mảng chuỗi hoặc null
+  keywords?: string[];
 
   @Prop()
-  image_url?: string;
+  video_url?: string;
 
   @Prop()
-  content?: string;
+  description?: string;
+
+  @Prop([{ type: { type: String, enum: ["text", "image"] }, value: String }]) // Có thể null
+  content?: { type: string; value: string }[];
 
   @Prop()
-  pubDate: Date;
+  pubDate: string;
+
+  @Prop()
+  pubDateTZ: string;
+
+  @Prop()
+  image_url: string;
+
+  @Prop()
+  source_id: string;
 
   @Prop()
   source_name: string;
 
+  @Prop()
+  source_url: string;
+
+  @Prop()
+  source_icon?: string;
+
+  @Prop({ default: "vietnamese" })
+  language: string;
+
   @Prop({ type: Types.ObjectId, ref: "Category", required: true })
-  category: Types.ObjectId; // Tham chiếu đến Category
+  category: Types.ObjectId;
 }
 
 export const ArticleSchema = SchemaFactory.createForClass(Article);
